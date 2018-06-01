e0a3ed445091b8553b8722bbf5e0a8e88d739117
Lenovo <Lenovo@DESKTOP-UAOUHUM>
2018-06-01 17:52:30 +0300

    TestForExpressionNew

---
+++ ExpressionNew/src/org/itstep/Factorial.java
@@ -0,0 +1,17 @@
+package org.itstep;
+
+public class Factorial {
+	
+	public int getFactorial(int x) {
+		if (x > 0 && x < 13) {
+			int result = 1;
+			for (int i = 2; i <= x; i++) {
+				result = result * i;
+			}
+			return result;
+		} else {
+			return 0;
+		}
+
+}
+}
---
+++ ExpressionNew/src/org/itstep/Function.java
@@ -0,0 +1,20 @@
+package org.itstep;
+
+public class Function {
+	
+public int x = 2;
+	
+	public int getFunction() {
+		int result = 0;
+		if((x > 0) && (x < 12)) {
+		Factorial factorial = new Factorial();
+		result =(x + 2)*factorial.getFactorial(x);
+		return result;
+		}else {
+			System.out.println("Incorrect number");
+			return 0;
+		}
+		
+	}
+
+}
